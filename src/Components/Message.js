9db37ef79eb87cf152db88be4526a92c964f1487
Ashley Johns <ashleypjohns@gmail.com>
2019-03-26 14:43:31 -0400

    create paper popup with message when click on bottle

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import bottle from "../bottle.png";
 
 class Message extends Component {
+	state = {
+		showMessage: false
+	};
+
+	toggleMessage = () => {
+		this.setState({
+			showMessage: !this.state.showMessage
+		});
+	};
+
 	render() {
 		return (
-			<section className="message">
-				<button className="bottle">
+			<section className="message-container">
+				<button className="bottle" onClick={this.toggleMessage}>
 					<img src={bottle} alt="bottle" />
 				</button>
 				{!this.props.message && (
 					<button onClick={this.props.getMessage}>Get Message</button>
 				)}
-				{this.props.message && <p>{this.props.message}</p>}
+				<TransitionGroup component="div" className="message">
+					{this.state.showMessage && (
+						<CSSTransition
+							classNames="msg"
+							timeout={{ enter: 500, exit: 500 }}
+						>
+							<p className="msg">{this.props.message}</p>
+						</CSSTransition>
+					)}
+				</TransitionGroup>
 			</section>
 		);
 	}
